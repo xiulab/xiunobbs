054db67c374a575c6bdeb1cf021e619da7a2f8e6
axiuno <axiuno@gmail.com>
2016-07-28 21:08:37 +0800

    nav

--- view/js/bbs.js
+++ view/js/bbs.js
@@ -1,4 +1,10 @@
-$('tr.thread').on('click', function() {
+$('.tap').on('click', function() {
 	var href = $(this).attr('href');
 	window.location = href;
 });
+$('li').on('click', function() {
+	var jthis = $(this);
+	var href = jthis.children('a').attr('href');
+	if(href) window.location = href;
+});
+
