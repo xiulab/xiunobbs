b89d40a064cd15db7f3a27e0b34ccb0380e12435
axiuno <axiuno@gmail.com>
2016-08-08 22:55:36 +0800

    .gitignore

--- log/20160808/admin_login.php
+++
@@ -1,1 +0,0 @@
-<?php exit;?>	2016-08-08 22:14:13	127.0.0.1	/admin/?index-login.htm	1	login successed. uid:1
--- log/20160808/admin_login_error.php
+++
@@ -1,1 +0,0 @@
-<?php exit;?>	2016-08-08 22:14:10	127.0.0.1	/admin/?index-login.htm	1	password error. uid:1 - ******14862c
