83b9bc6ff8321118634edb7e9044e53d567b2706
axiuno <axiuno@gmail.com>
2016-08-08 16:57:27 +0800

    fix

--- conf/smtp.conf.php
+++ conf/smtp.conf.php
@@ -8,13 +8,5 @@ return array (
     'user' => 'xxxx',
     'pass' => 'xxxx',
   ),
-  1 => 
-  array (
-    'email' => 'xxxx@sina.com',
-    'host' => 0,
-    'port' => 25,
-    'user' => 'xxxx22',
-    'pass' => 'xxxx33',
-  ),
 );
 ?>
