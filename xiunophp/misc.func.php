9bc7048d315a92cb73dcc6449473a48998b8e997
axiuno <axiuno@gmail.com>
2016-08-09 17:29:04 +0800

    fix pagination() a miss link class

--- xiunophp/misc.func.php
+++ xiunophp/misc.func.php
@@ -338,7 +338,7 @@ function pagination($url, $totalnum, $page, $pagesize = 20) {
 	//$s = '<ul class="pagination">';
 	$s = '';
 	$page != 1 && $s .= '<li class="page-item"><a href="'.str_replace('{page}', $page-1, $url).'" class="page-link">◀</a></li>'."\r\n";
-	if($start > 1) $s .= '<li class="page-item"><a href="'.str_replace('{page}', 1, $url).'">1 '.($start > 2 ? '... ' : '').'</a></li>'."\r\n";
+	if($start > 1) $s .= '<li class="page-item"><a href="'.str_replace('{page}', 1, $url).'" class="page-link">1 '.($start > 2 ? '... ' : '').'</a></li>'."\r\n";
 	for($i=$start; $i<=$end; $i++) {
 		if($i == $page) {
 			$s .= '<li class="page-item active"><a href="'.str_replace('{page}', $i, $url).'" class="page-link">'.$i.'</a></li>'."\r\n";// active
