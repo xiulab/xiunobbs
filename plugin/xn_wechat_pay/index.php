e3dd3daa214f19d0a426e3b3f6fe11d95cc6ac6b
axiuno <axiuno@gmail.com>
2016-08-20 21:07:08 +0800

    fix

---
+++ plugin/xn_wechat_pay/index.php
@@ -0,0 +1,10 @@
+<?php
+define('DEBUG', 1);
+define('BASE_HREF', '../../');
+define('SKIP_ROUTE', true); // 跳过路由处理，否则 index.php 中会中断流程
+chdir('../../');
+include './index.php';
+empty( $user ) AND http_location(url('user-login'));
+include './plugin/xn_wechat_pay/model/wechatpay.class.php';
+include './plugin/xn_wechat_pay/route/index.php';
+?>
