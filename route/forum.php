1dda4d9c101327a132a7c72ac1aa603b36d68ffa
axiuno <axiuno@gmail.com>
2016-08-26 22:03:55 +0800

    fix forum top

--- route/forum.php
+++ route/forum.php
@@ -12,6 +12,8 @@ empty($forum) AND message(3, lang('forum_not_exists'));
 forum_access_user($fid, $gid, 'allowread') OR message(-1, lang('insufficient_visit_forum_privilege'));
 $pagesize = $conf['pagesize'];
 
+$toplist = thread_top_find($fid);
+
 // 从默认的地方读取主题列表
 $thread_list_from_default = 1;
 
@@ -22,6 +24,7 @@ if($thread_list_from_default) {
 	$threadlist = thread_find_by_fid($fid, $page, $pagesize);
 }
 
+$threadlist = $toplist + $threadlist;
 
 $header['title'] = $forum['seo_title'] ? $forum['seo_title'] : $forum['name'].'-'.$conf['sitename'];
 $header['mobile_title'] = $forum['name'];
