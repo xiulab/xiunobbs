010d391814b66330002227653e9cd514edd1aa26
axiuno <axiuno@gmail.com>
2018-01-05 16:05:28 +0800

    bs4 beta3

--- plugin/xn_tag/hook/post_update_post_end.php
+++ plugin/xn_tag/hook/post_update_post_end.php
@@ -48,11 +48,15 @@
 			$tagids_old = tag_thread_find_tagid_by_tid($tid);
 			$tag_id_delete = array_diff($tagids_old, $tagids_new);
 			$tag_id_add = array_diff($tagids_new, $tagids_old);
-			foreach($tag_id_delete as $tagid) {
-				$tagid AND tag_thread_delete($tagid, $tid);
+			if($tag_id_delete) {
+				foreach($tag_id_delete as $tagid) {
+					$tagid AND tag_thread_delete($tagid, $tid);
+				}
 			}
-			foreach($tag_id_add as $tagid) {
-				$tagid AND tag_thread_create($tagid, $tid);
+			if($tag_id_add) {
+				foreach($tag_id_add as $tagid) {
+					$tagid AND tag_thread_create($tagid, $tid);
+				}
 			}
 			thread_update($tid, array('tagids'=>'', 'tagids_time'=>0));
 			/*
