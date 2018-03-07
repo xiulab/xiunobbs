b34a6c165e314f6546adf70c807e5ac6a3befa1a
axiuno <axiuno@gmail.com>
2018-03-07 09:35:09 +0800

    xn_digest for 4.0.3

--- plugin/xn_digest/hook/my_start.php
+++ plugin/xn_digest/hook/my_start.php
@@ -6,11 +6,5 @@ if($action == 'digest') {
 	$pagination = pagination(url("user-$uid-{page}-1"), $digests, $page, $pagesize);
 	$threadlist = thread_digest_find_by_uid($uid, $page, $pagesize);
 	
-	$active = 'thread';
-	if($ajax) {
-		foreach($threadlist as &$thread) $thread = thread_safe_info($thread);
-		message(0, $threadlist);
-	} else {
-		include _include(APP_PATH.'plugin/xn_digest/view/htm/my_digest.htm');
-	}
+	include _include(APP_PATH.'plugin/xn_digest/view/htm/my_digest.htm');
 }
