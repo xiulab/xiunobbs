31d278c6128711ad3228ae629a29be69b78f0280
axiuno <axiuno@gmail.com>
2018-04-16 10:51:36 +0800

    deprecated each()

--- xiunophp/xn_send_mail.func.php
+++ xiunophp/xn_send_mail.func.php
@@ -1824,7 +1824,8 @@ class PHPMailer {
     $eol = "\r\n";
     $escape = '=';
     $output = '';
-    while( list(, $line) = each($lines) ) {
+    //while( list(, $line) = each($lines) ) {
+    foreach ($lines as $line) {
       $linlen = strlen($line);
       $newline = '';
       for($i = 0; $i < $linlen; $i++) {
